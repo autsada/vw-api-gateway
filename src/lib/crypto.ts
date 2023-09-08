7a9a57357c4e290f4e2172da876745abcc4973cf
autsada <autsada.t@gmail.com>
2023-09-08 13:53:33 +0700

    updated variable name

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -1,10 +1,10 @@
 import CryptoJs from "crypto-js"
 
-const { ENCRYPT_KEY } = process.env
+const { PUBSUB_ENCRYPT_KEY } = process.env
 
 export function decryptString(text: string) {
   console.time("string-decrypt")
-  const bytes = CryptoJs.AES.decrypt(text, ENCRYPT_KEY!)
+  const bytes = CryptoJs.AES.decrypt(text, PUBSUB_ENCRYPT_KEY!)
 
   console.timeEnd("string-decrypt")
   return bytes.toString(CryptoJs.enc.Utf8)
