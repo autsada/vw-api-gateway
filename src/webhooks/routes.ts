2f70f6bf6070b4228870f4d26b7d39614ae9cbc6
autsada <autsada.t@gmail.com>
2023-09-13 09:41:55 +0700

    remove some routes for test env

--- src/webhooks/routes.ts
+++ src/webhooks/routes.ts
@@ -15,8 +15,8 @@ export const router = express.Router()
 
 router.post("/address-updated", onAddressUpdated)
 router.get("/cloudflare", getTranscodeWebhook)
-router.post("/cloudflare", createTranscodeWebhook)
-router.delete("/cloudflare", deleteTranscodeWebhook)
+// router.post("/cloudflare", createTranscodeWebhook)
+// router.delete("/cloudflare", deleteTranscodeWebhook)
 router.post(
   "/cloudflare/finished",
   validateCloudflareSignature,
