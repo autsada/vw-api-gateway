0f76b26d6e6fa62784f5fb0470b05e4da80576cf
autsada <autsada.t@gmail.com>
2023-10-12 11:38:17 +0700

    adjusted blogs fetch quanity to fix frontend error

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
 export const FETCH_QTY = 30 // Number of rows to be fetched each time
-export const BLOG_FETCH_QTY = 10 // Number of rows to be fetched each time
+export const BLOG_FETCH_QTY = 2 // For blogs we need to set to 2 as greater number causes error on the frontend
